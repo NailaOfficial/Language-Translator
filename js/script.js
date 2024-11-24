a9c0353f6a73c3ebe95b5fb8fd7bec797b4feb5b
NailaOfficial <nameerofficial@gmail.com>
2024-11-24 20:00:57 +0500

    Language-Translator

---
+++ js/script.js
@@ -0,0 +1,97 @@
+const fromText = document.querySelector(".from-text"),
+      toText = document.querySelector(".to-text"),
+      exchangeIcon = document.querySelector(".exchange"),
+      selectTag = document.querySelectorAll("select"),
+      icons = document.querySelectorAll(".row i"),
+      translateBtn = document.querySelector("button");
+
+let voices = [];
+function populateVoices() {
+    voices = speechSynthesis.getVoices();
+}
+
+if (speechSynthesis.onvoiceschanged !== undefined) {
+    speechSynthesis.onvoiceschanged = populateVoices;
+}
+
+selectTag.forEach((tag, id) => {
+  for (let country_code in countries) {
+    let selected = id == 0 ? country_code == "en-GB" ? "selected" : "" : country_code == "ur-PK" ? "selected" : "";
+    let option = `<option ${selected} value="${country_code}">${countries[country_code]}</option>`;
+    tag.insertAdjacentHTML("beforeend", option);
+  }
+});
+
+exchangeIcon.addEventListener("click", () => {
+  let tempText = fromText.value,
+      tempLang = selectTag[0].value;
+  fromText.value = toText.value;
+  toText.value = tempText;
+  selectTag[0].value = selectTag[1].value;
+  selectTag[1].value = tempLang;
+});
+
+fromText.addEventListener("keyup", () => {
+  if (!fromText.value) {
+    toText.value = "";
+  }
+});
+
+translateBtn.addEventListener("click", () => {
+  let text = fromText.value.trim(),
+      translateFrom = selectTag[0].value,
+      translateTo = selectTag[1].value;
+  if (!text) return;
+  
+  toText.setAttribute("placeholder", "Translating...");
+  
+  let apiUrl = `https://api.mymemory.translated.net/get?q=${text}&langpair=${translateFrom}|${translateTo}`;
+  
+  fetch(apiUrl)
+    .then(res => res.json())
+    .then(data => {
+      toText.value = data.responseData.translatedText;
+      toText.setAttribute("placeholder", "Translation");
+    })
+    .catch(err => {
+      toText.value = "Error in translation!";
+      toText.setAttribute("placeholder", "Translation");
+    });
+});
+
+icons.forEach(icon => {
+  icon.addEventListener("click", ({ target }) => {
+    if (!fromText.value || !toText.value) return;
+
+    let utterance;
+
+    if (target.classList.contains("fa-copy")) {
+      if (target.id == "from") {
+        navigator.clipboard.writeText(fromText.value);
+      } else {
+        navigator.clipboard.writeText(toText.value);
+      }
+    } 
+    
+    else {
+      if (target.id == "from") {
+        utterance = new SpeechSynthesisUtterance(fromText.value);
+        utterance.lang = selectTag[0].value;
+      } else {
+        utterance = new SpeechSynthesisUtterance(toText.value);
+        utterance.lang = selectTag[1].value;
+      }
+      if (utterance.lang === "en-GB") {
+        utterance.voice = voices.find(voice => voice.lang === "en-GB");
+      } else if (utterance.lang === "ur-PK") {
+        utterance.voice = voices.find(voice => voice.lang === "ur-PK");
+      }
+
+      if (!utterance.voice) {
+        utterance.voice = voices[0];
+      }
+
+      speechSynthesis.speak(utterance);
+    }
+  });
+});
